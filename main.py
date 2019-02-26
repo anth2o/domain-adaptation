8172ac7b9c43436e83b48337f7c7147d62415cc8
Antoine <ahoorelbeke@gmail.com>
2019-02-26 15:48:26 +0100

    Add callbacks

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from model.model import CustomModel
 from model.preprocessing import get_data
 
 BATCH_SIZE = 32
-EPOCHS = 50
+EPOCHS = 100
 NUM_CLASSES = 10
 SAVE_DIR = 'saved_models/'
-MODEL_NAME = 'cifar10-cnn.h5'
+MODEL_NAME = 'cnn_train_svhn.h5'
 SUBSET = None
+LOG_FILE = 'logs/cnn_train_svhn.log'
 
 (x_train, y_train), (x_test, y_test) = get_data(num_classes=NUM_CLASSES, subset=SUBSET)
 model = CustomModel()
 model._build(num_classes=NUM_CLASSES)
 model._compile()
-model._fit(x_train, y_train, x_test, y_test, batch_size=BATCH_SIZE, epochs=EPOCHS)
+model._fit(x_train, y_train, x_test, y_test, batch_size=BATCH_SIZE, epochs=EPOCHS, log_file=LOG_FILE)
 model._save(save_dir=SAVE_DIR, model_name=MODEL_NAME)
 model._evaluate(x_test, y_test)
--- model/model.py
+++ model/model.py
@@ -1,13 +1,14 @@
 import keras
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Input
 from keras.models import Model
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping, CSVLogger
 import os
 
 
 class CustomModel:
     def __init__(self):
         self.model = None
-        
+
     def _build(self, num_classes):
         inputs = Input(shape=(32, 32, 3))
         x = Conv2D(32, (3, 3), padding='same', activation='relu')(inputs)
@@ -30,19 +31,24 @@ class CustomModel:
     def _compile(self):
         if not self.model:
             raise Exception("Trying to compile model but it isn't built")
-        opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+        opt = keras.optimizers.rmsprop(lr=10e-3, decay=1e-6)
         self.model.compile(loss='categorical_crossentropy',
                     optimizer=opt,
                     metrics=['accuracy'])
 
-    def _fit(self, x_train, y_train, x_test, y_test, batch_size=32, epochs=5):
+    def _fit(self, x_train, y_train, x_test, y_test, batch_size=32, epochs=5, log_file='logs/base.log'):
         if not self.model:
             raise Exception("Trying to fit model but it isn't built")
+        early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=10, restore_best_weights=True)
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                              patience=5, min_lr=10e-7)
+        csv_logger = CSVLogger(log_file)
         self.model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           validation_data=(x_test, y_test),
-          shuffle=True)
+          shuffle=True,
+          callbacks=[reduce_lr, early_stopping, csv_logger],)
 
     def _save(self, save_dir, model_name):
         if not self.model:
