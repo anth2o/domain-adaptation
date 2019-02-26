063465f0c635958c7e65dfab8a4c5e07f98ddcb1
Antoine <ahoorelbeke@gmail.com>
2019-02-26 15:32:29 +0100

    Ready to launch training

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from model.model import CustomModel
 from model.preprocessing import get_data
 
 BATCH_SIZE = 32
-EPOCHS = 100
+EPOCHS = 50
 NUM_CLASSES = 10
 SAVE_DIR = 'saved_models/'
-MODEL_NAME = 'base.h5'
-SUBSET = 10
+MODEL_NAME = 'cifar10-cnn.h5'
+SUBSET = None
 
 (x_train, y_train), (x_test, y_test) = get_data(num_classes=NUM_CLASSES, subset=SUBSET)
 model = CustomModel()
--- model/model.py
+++ model/model.py
@@ -5,10 +5,10 @@ import os
 
 
 class CustomModel:
+    def __init__(self):
+        self.model = None
+        
     def _build(self, num_classes):
-    #     model.add(Lambda(augment_2d,
-    #                     input_shape=x_train.shape[1:],
-    #                     arguments={'rotation': 8.0, 'horizontal_flip': True}))
         inputs = Input(shape=(32, 32, 3))
         x = Conv2D(32, (3, 3), padding='same', activation='relu')(inputs)
         x = Conv2D(32, (3, 3), activation='relu')(x)
@@ -28,12 +28,16 @@ class CustomModel:
         self.model = Model(inputs=inputs, outputs=predictions)
 
     def _compile(self):
+        if not self.model:
+            raise Exception("Trying to compile model but it isn't built")
         opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
         self.model.compile(loss='categorical_crossentropy',
                     optimizer=opt,
                     metrics=['accuracy'])
 
     def _fit(self, x_train, y_train, x_test, y_test, batch_size=32, epochs=5):
+        if not self.model:
+            raise Exception("Trying to fit model but it isn't built")
         self.model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
@@ -41,6 +45,8 @@ class CustomModel:
           shuffle=True)
 
     def _save(self, save_dir, model_name):
+        if not self.model:
+            raise Exception("Trying to save model but it isn't built")
         if not os.path.isdir(save_dir):
             os.makedirs(save_dir)
         model_path = os.path.join(save_dir, model_name)
@@ -48,6 +54,8 @@ class CustomModel:
         print('Saved trained model at %s ' % model_path)
 
     def _evaluate(self, x_test, y_test):
+        if not self.model:
+            raise Exception("Trying to evaluate model but it isn't built")
         scores = self.model.evaluate(x_test, y_test, verbose=1)
         print('Test loss:', scores[0])
         print('Test accuracy:', scores[1])
